28027a4cf1566dcc6d7ab5bdf40fa145dd0eb6ef
Brandi Williams <brandimichelle.0911@gmail.com>
2019-04-12 14:09:05 -0700

    Adding to seed file

--- seeders/20160612205341-populate-posts-authors.js
+++ seeders/20160612205341-populate-posts-authors.js
@@ -2,52 +2,38 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('Person', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
     return queryInterface.bulkInsert('authors', [
       { firstName: 'Keith',
         lastName: 'Fuller',
-        bio: 'Key contributor to various newspapers in the Greater Puget Sound area. Father. Bike enthusiast.',
-        createdAt: new Date(),
-        updatedAt: new Date() },
+        bio: 'Key contributor to various newspapers in the Greater Puget Sound area. Father. Bike enthusiast.'
+      },
+      { firstName: 'Bobby',
+        lastName: 'Bobbington',
+        bio: 'Adventurer, biking and hiking enthusiast.'
+      },
       { firstName: 'Misty',
         lastName: 'Owens',
-        bio: 'Blogger in favor of public transit and community building in First Hill and Capitol Hill.',
-        createdAt: new Date(),
-        updatedAt: new Date() }
+        bio: 'Blogger in favor of public transit and community building in First Hill and Capitol Hill.'
+      }
     ], { returning: true }).then(function(authors) {
       return queryInterface.bulkInsert('posts', [
         { title: 'Bike usage in Seattle grows as city plans new lanes.',
           content: 'Bike lanes in the downtown area, as well as the surrounding areas, are being installed.',
-          authorId: authors[0].id,
-          createdAt: new Date(),
-          updatedAt: new Date()
+          authorId: authors[0].id
         },
         { title: 'Local Events: Capitol Hill Art Walk',
           content: 'Celebrate community and expression at the next Capitol Hill Art Walk.',
-          authorId: authors[0].id,
-          createdAt: new Date(),
-          updatedAt: new Date()
+          authorId: authors[0].id
+        },
+        { title: 'Things To Do: Hike Rattlesnake Ridge',
+          content: 'This 2 mile hike is difficult enough to feel like an accomplishment, easy enough for most people to handle, and has a great view at the top.',
+          authorId: authors[1].id
         }
       ]);
     });
   },
 
   down: function (queryInterface, Sequelize) {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('Person', null, {});
-    */
+    return queryInterface.bulkDelete('authors', null, {});
   }
 };
