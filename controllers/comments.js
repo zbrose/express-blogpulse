8854992738e3cdcdff9db0fe010a942258b50689
brandiw <brandimichelle.0911@gmail.com>
2019-04-16 09:43:40 -0700

    Finished regular deliverable

--- controllers/comments.js
+++ controllers/comments.js
@@ -3,7 +3,14 @@ var db = require('../models')
 var router = express.Router()
 
 router.post('/', (req, res) => {
-  res.send(req.body)
+  db.comment.create(req.body)
+  .then((createdComment) => {
+    res.redirect('/articles/' + req.body.articleId)
+  })
+  .catch((err) => {
+    console.log('Error in POST /comments', err)
+    res.send('An error occurred on the server; please check your logs.')
+  })
 })
 
 module.exports = router
