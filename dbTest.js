3cef61e5d160f9743573c60843fcf7f5e3ac20ac
009kings <009kings@gmail.com>
2020-06-24 15:24:46 -0700

    part one done

---
+++ dbTest.js
@@ -0,0 +1,15 @@
+let db = require('./models')
+
+// db.comment.create({
+//   name: 'Paul Allen',
+//   content: 'This is really neat! Thanks for posting.',
+//   articleId: 1
+// })
+// .then(comment => {
+//   console.log(comment.get())
+// })
+
+// db.article.findByPk(1, { include: [db.comment]})
+// .then(article => {
+//   console.log(article.comments)
+// })
--- models/article.js
+++ models/article.js
@@ -8,6 +8,7 @@ module.exports = (sequelize, DataTypes) => {
   article.associate = function(models) {
     // associations can be defined here
     models.article.belongsTo(models.author)
+    models.article.hasMany(models.comment)
   }
   return article
 }
